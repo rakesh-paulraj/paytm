b4358cd8976f5f4963814df39040adf1bb41cf5e
rakesh-paulraj <rakeshpaulraj2004@gmail.com>
2024-01-26 18:24:32 +0530

    Schema created

---
+++ backend/database/db.js
@@ -0,0 +1,37 @@
+const mongoose=require("mongoose");
+const {Schema,model}=require ("mongoose");
+
+mongoose.connect("mongodb+srv://admin:fQ8qLRJqWtSwH33G@cluster0.mmielod.mongodb.net/paytm");
+
+
+const userschema=mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    firstname : {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    secondname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
+})
+const User=mongoose.model('User',userschema);
+
+module.exports={User};
