709eca0f729b3f9091024717a05d0c86553c325b
rakesh-paulraj <rakeshpaulraj2004@gmail.com>
2024-02-01 06:38:00 +0530

    Main pages created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import{Signup} from "./pages/Signup";
-import{Dashboard} from "./pages/Dashboard";
-import{SendMoney}from "./pages/SendMoney";
-import{ Signin } from "./pages/Signin";
+  import { BrowserRouter,Routes,Route } from "react-router-dom";
+  import{Signup} from "./pages/Signup";
+  import{Dashboard} from "./pages/Dashboard";
+  import{SendMoney}from "./pages/SendMoney";
+  import{ Signin } from "./pages/Signin";
+  import "./index.css";
 
-function App() {<>
-<BrowserRouter>
-<Routes>
-  <Route path="/signin" element={<Signin />}/>
-  <Route path="/signup" element={<Signup/>}/>
-  <Route path="/dashboard" element={<Dashboard/>}/>
-  <Route path="/send" elemnt={<SendMoney/>}/>
-</Routes>
-</BrowserRouter>
-</>
-}
+  function App() {
+  
+  
+   return <div><BrowserRouter>
+  <Routes>
+    <Route path="/signin" element={<Signin />}/>
+    <Route path="/signup" element={<Signup/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
+    <Route path="/send" element={<SendMoney/>}/>
+  </Routes>
+  </BrowserRouter>
+  </div>
+  
+  }
 
-export default App 
+  export default App 
 
