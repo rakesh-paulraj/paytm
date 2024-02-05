70777aaea6f30eae1c06b8d4b15aba49fc2e6699
rakesh-paulraj <rakeshpaulraj2004@gmail.com>
2024-02-05 06:46:45 +0530

    resolved the dashboard problem

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -4,13 +4,16 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 
+
 export const Users = () => {
   
     const [users, setUsers] = useState([]);
     const [filter, setFilter] = useState("");
 
     useEffect(() => {
-        axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + filter)
+        const token=localStorage.getItem("token");
+        axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + filter,{
+            headers:{Authorization:"Bearer"+token,},})
             .then(response => {
                 setUsers(response.data.user)
             })
@@ -21,8 +24,10 @@ export const Users = () => {
             Users
         </div>
         <div className="my-2">
-            <input onChange={(e) => {
-                setFilter(e.target.value)
+            <input onChange={(e) => {if (e.target.value==""){
+                setFilter("tniersntufytnesntnrit")
+            }else{
+                setFilter(e.target.value)}
             }} type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
         </div>
         <div>
@@ -38,12 +43,12 @@ function User({user}) {
         <div className="flex">
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
                 <div className="flex flex-col justify-center h-full text-xl">
-                { user.firstName && user.firstName[0]}
+                <span className="user-icon">{ user.firstname[0]}</span>
                 </div>
             </div>
             <div className="flex flex-col justify-center h-ful">
                 <div>
-                { user.firstName && user.lastName && `${user.firstName} ${user.lastName}`}
+               <span> {  `${user.firstname} `}</span>
                 </div>
             </div>
         </div>
